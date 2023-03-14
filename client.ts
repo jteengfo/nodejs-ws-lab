b7c988c55f132b8e3d5d68b9d1db64c1bed08916
James T <jamesang@ualberta.ca>
2023-03-14 09:52:35 -0600

    topdown

--- client.ts
+++ client.ts
@@ -8,6 +8,28 @@ interface ICoords {
   y: number;
 }
 
+function uuid(
+  a?: any               // placeholder
+): string {
+  return a              // if the placeholder was passed, return
+    ? (                 // a random number from 0 to 15
+      a ^               // unless b is 8,
+      Math.random()     // in which case
+      * 16              // a random number from
+      >> a / 4          // 8 to 11
+    ).toString(16)      // in hexadecimal
+    : (                 // or otherwise a concatenated string:
+      1e7.toString() +  // 10000000 +
+      -1e3 +            // -1000 +
+      -4e3 +            // -4000 +
+      -8e3 +            // -80000000 +
+      -1e11             // -100000000000,
+    ).replace(          // replacing
+      /[018]/g,         // zeroes, ones, and eights with
+      uuid              // random hex digits
+    )
+}
+
 const DEBUG = false; // Render debug physics entities
 
 class GameScene extends Phaser.Scene {
@@ -16,7 +38,10 @@ class GameScene extends Phaser.Scene {
 
   private VELOCITY = 100;
   private wsClient?: WebSocket;
-  private player?: Phaser.GameObjects.Sprite;
+
+  // private player?: Phaser.GameObjects.Sprite;
+  private id = uuid();
+  private players: {[key: string]: Phaser.GameObjects.Sprite} = {};
   private leftKey?: Phaser.Input.Keyboard.Key;
   private rightKey?: Phaser.Input.Keyboard.Key;
   private upKey?: Phaser.Input.Keyboard.Key;
